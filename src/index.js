e414583749fb02beae261b059c9ece1f4a03bbcc
minhnghia2208 <nghiapham@umass.edu>
2021-06-10 16:51:59 +0700

    working pretty good

--- src/index.js
+++ src/index.js
@@ -3,70 +3,88 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import { StylesProvider } from "@material-ui/core/styles";
 import './index.css';
+import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
+import AddLocationIcon from '@material-ui/icons/AddLocation';
+// import AdbIcon from '@material-ui/icons/Adb';
 
-class Square extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = { value: this.props.value};
-  }
-  handleClick() {
-    this.setState({value: !this.state.value});
-  }
-  render(){
-    return (
-      <StylesProvider injectFirst>
-        <Button 
-          onClick={() => this.handleClick()}
-          className= {this.state.value? "button_before": "button_after"}>
-          {this.props.value}
-        </Button>
-      </StylesProvider>
-    )}
-}
 class Board extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      squares: new Array(9).fill(new Array(9).fill(true)),
-      clicked: false,
+      squares: new Array(9).fill(0).map(row => new Array(9).fill(true)),
+      x: 0,
+      y: 0,
     };
   }
-  renderSquare(i, j){
-    return (i===0 && j ==0) ? <div><Square value = {false}/></div>:
-    <div>{new Array(9).fill(<Square value = {true}/>)}</div>;
+  handleClick(i, j){
+    if ((i === this.state.x && j === this.state.y + 1) ||
+      (i === this.state.x && j === this.state.y - 1) ||
+      (i === this.state.x + 1 && j === this.state.y) ||
+      (i === this.state.x - 1 && j === this.state.y)){
+      if (i === this.state.squares.length-1 && j === this.state.squares.length-1)
+        return this.handleFinish();
+      if (!this.state.squares[i][j]){
+        alert("Nuh Uh, Can't go bacc mah man");
+        return;
+      }
+      var new_squares = this.state.squares.slice();
+      new_squares[i][j] = !new_squares[i][j]
+      this.setState({
+        squares: new_squares,
+        x: i,
+        y: j,
+      });
+      return;
+    }
+    alert("Wait, that is illegal")  
+  }
+  handleFinish(){
+    alert("You win DUN DUN")
+  }
+  handleIcon(i, j){
+    return !this.state.squares[i][j]? 
+    (i === this.state.x && j === this.state.y)? <AirportShuttleIcon/>: null:
+    (i === this.state.squares.length-1 && j === this.state.squares.length-1)?<AddLocationIcon/>: null;
+  }
+  handleAI(){
 
   }
-  render(){
-    var rows = new Array(9);
-    for (let i = 0; i < rows.length; i++){    
-      rows[i] = i===0 ? <div><Square value = {false}/>{new Array(8).fill(<Square value = {true}/>)}</div>:
-      <div>{new Array(9).fill(<Square value = {true}/>)}</div>;
+  renderSquare(i, j){
+     return (
+      <StylesProvider injectFirst>
+        <Button 
+          onClick={() => this.handleClick(i, j)}
+          className= {
+            (i === 0 && j === 0)?"button_after":
+            (i === this.state.squares.length-1 && j === this.state.squares.length-1)? "button_icon":
+            this.state.squares[i][j]?"button_before":
+            (i === this.state.x && j === this.state.y)?"button_icon": "button_after"}>
+              {this.handleIcon(i, j)}
+        </Button>
+      </StylesProvider>
+     )
+  }
+  renderBoard(){
+    var columns = new Array(9).fill(true);
+    var rows = new Array(9).fill(0).map(() => new Array(9).fill(true));
+    for (let i = 0; i < this.state.squares.length; i++){
+      for (let j = 0; j < this.state.squares[i].length; j++){
+        rows[i][j] = this.renderSquare(i, j);
+      }
+      columns[i] = <div>{rows[i]}</div>
     }
+    return columns;
+  }
+  render(){
     return (
       <div>
-        <h1 className='name'>EDVENTUR UFF DA SNAKEE</h1>
-        <div className='center'>{rows}</div>
+        <h1 className='name'>Ride Bus, Ride</h1>
+        <div className='center'>{this.renderBoard()}</div>
       </div>
     )
   }
-
 }
 ReactDOM.render(
   <html><Board/></html>,
   document.getElementById('root')
 );
-
-// Square Render
-// render(){
-//   var rows = new Array(9);
-//   for (let i = 0; i < rows.length; i++){    
-//     rows[i] = i===0 ? <div><Square value = {false}/>{new Array(8).fill(<Square value = {true}/>)}</div>:
-//     <div>{new Array(9).fill(<Square value = {true}/>)}</div>;
-//   }
-//   return (
-//     <div>
-//       <h1 className='name'>EDVENTUR UFF DA SNAKEE</h1>
-//       <div className='center'>{rows}</div>
-//     </div>
-//   )
-// }
